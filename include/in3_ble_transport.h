5da1be6088ec5d9992c793072c6a5e398a1acdcc
chirag-parmar <chirag.parmar@slock.it>
2019-09-24 15:14:11 +0200

    merge ble and project makefile

--- include/in3_ble_transport.h
+++ include/in3_ble_transport.h
@@ -2,5 +2,5 @@
   #define IN3_BLE_TRANSPORT_H_
   #include <client.h>   // the core client
   in3_ret_t transport_ble(char **urls, int urls_len, char *payload, in3_response_t *result);
-  void transport_init();
+  void transport_ble_init();
 #endif
